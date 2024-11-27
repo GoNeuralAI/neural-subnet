0fdaa7f8506d485c9ca2bfb96d5dfe410486f1a1
berose <aidream2100@gmail.com>
2024-11-27 00:45:04 +0000

    vfeat/validation error

--- validation/validation_endpoint.py
+++ validation/validation_endpoint.py
@@ -22,57 +22,59 @@ class Validation:
         self.init_model()
         
     def validate(self, data: ValidateRequest):
-        print("----------------- Validation started -----------------")
-        start = time.time()
-        prompt = data.prompt + " " + EXTRA_PROMPT
-        id = data.uid
-        
-        rendered_images, before_images = render(prompt, id)
-        
-        prev_img_path = os.path.join(DATA_DIR, f"{data.uid}/preview.png")
-        prev_img = load_image(prev_img_path)
-        
-        Q0 = self.quality_model.compute_quality(prev_img_path)
-        print(f"Q0: {Q0}")
-        
-        S0 = self.text_model.compute_clip_similarity_prompt(prompt, prev_img_path) if Q0 > 0.4 else 0
-        print(f"S0: {S0} - taken time: {time.time() - start}")
-        if S0 < 0.23:
-            return ValidateResponse(score=0)
+        try:
+            print("----------------- Validation started -----------------")
+            start = time.time()
+            prompt = data.prompt + " " + EXTRA_PROMPT
+            id = data.uid
             
-        Ri = self.detect_outliers([self.image_model.compute_clip_similarity(prev_img, img) for img in rendered_images])
-        
-        Si = self.detect_outliers([self.text_model.compute_clip_similarity_prompt(prompt, before_image) for before_image in before_images])
-        
-        print(f"R0: taken time: {time.time() - start}")
-        
-        Qi = self.detect_outliers([self.quality_model.compute_quality(img) for img in before_images])
-        
-        S_geo = np.exp(np.log(Si).mean())
-        R_geo = np.exp(np.log(Ri).mean())
-        Q_geo = np.exp(np.log(Qi).mean())
-        
-        print("---- Rendered images similarities with preview image ---")
-        print(Ri)
-        print(f"R_geo: {R_geo}")
-        
-        print("---- Rendered images similarities with text prompt ----")
-        print(Si)
-        print(f"S_geo: {S_geo}")
-        
-        print("---- Rendered images quality ----")
-        print(Qi)
-        print(f"Q_geo: {Q_geo}")
-        
-        total_score = S0 * 0.2 + S_geo * 0.4 + R_geo * 0.3 + Q_geo * 0.1
-        
-        print(f"---- Total Score: {total_score} ----")
-        
-        if total_score < 0.35:
+            rendered_images, before_images = render(prompt, id)
+            
+            prev_img_path = os.path.join(DATA_DIR, f"{data.uid}/preview.png")
+            prev_img = load_image(prev_img_path)
+            
+            Q0 = self.quality_model.compute_quality(prev_img_path)
+            print(f"Q0: {Q0}")
+            
+            S0 = self.text_model.compute_clip_similarity_prompt(prompt, prev_img_path) if Q0 > 0.4 else 0
+            print(f"S0: {S0} - taken time: {time.time() - start}")
+            if S0 < 0.23:
+                return ValidateResponse(score=0)
+                
+            Ri = self.detect_outliers([self.image_model.compute_clip_similarity(prev_img, img) for img in rendered_images])
+            
+            Si = self.detect_outliers([self.text_model.compute_clip_similarity_prompt(prompt, before_image) for before_image in before_images])
+            
+            print(f"R0: taken time: {time.time() - start}")
+            
+            Qi = self.detect_outliers([self.quality_model.compute_quality(img) for img in before_images])
+            
+            S_geo = np.exp(np.log(Si).mean())
+            R_geo = np.exp(np.log(Ri).mean())
+            Q_geo = np.exp(np.log(Qi).mean())
+            
+            print("---- Rendered images similarities with preview image ---")
+            print(Ri)
+            print(f"R_geo: {R_geo}")
+            
+            print("---- Rendered images similarities with text prompt ----")
+            print(Si)
+            print(f"S_geo: {S_geo}")
+            
+            print("---- Rendered images quality ----")
+            print(Qi)
+            print(f"Q_geo: {Q_geo}")
+            
+            total_score = S0 * 0.2 + S_geo * 0.4 + R_geo * 0.3 + Q_geo * 0.1
+            
+            print(f"---- Total Score: {total_score} ----")
+            
+            if total_score < 0.35:
+                return ValidateResponse(score=0)
+            return ValidateResponse(score=total_score)
+        except Exception as e:
+            print(f"Error in validation process: {str(e)}")
             return ValidateResponse(score=0)
-        return ValidateResponse(
-            score=total_score
-        )
         
     def detect_outliers(self, data, threshold=1.1):
         # Calculate Q1 and Q3
