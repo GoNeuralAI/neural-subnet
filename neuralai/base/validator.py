cedf9cc7d4b5b5e755a564c9d436063515683363
berose <aidream2100@gmail.com>
2024-12-09 14:56:29 +0000

    incentive curve function

--- neuralai/base/validator.py
+++ neuralai/base/validator.py
@@ -254,7 +254,7 @@ class BaseValidatorNeuron(BaseNeuron):
         ranks.sort(key=lambda x: x[0])
         
         # self.scores = [(math.exp(-0.03 * rank) if score > 0 else 0) for id, rank, score in ranks]
-        self.scores = [(score * score if score > 0 else 0) for score in self.base_scores]
+        self.scores = [(score ** 4 if score > 2e-1 else 0) for score in self.base_scores]
         
         bt.logging.info(f"scores: {self.scores}")
 
