ba0f9bece788057b2f6e1baf3d14d0f1c04d800c
berose <aidream2100@gmail.com>
2025-04-03 10:17:36 +0000

    release version

--- neuralai/__init__.py
+++ neuralai/__init__.py
@@ -20,7 +20,7 @@
 # TODO(developer): Change this value when updating your code base.
 # Define the version of the template module.
 
-__version__ = "2.5.4"
+__version__ = "2.5.5"
 
 version_split = __version__.split(".")
 __spec_version__ = (
